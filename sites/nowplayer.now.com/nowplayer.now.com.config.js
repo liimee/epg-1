7b02388928d7cca3399eb4433fde301d9a7b2d0a
liimee <git.taaa@fedora.email>
2023-08-01 21:05:36 +0700

    hmm

--- sites/nowplayer.now.com/nowplayer.now.com.config.js
+++ sites/nowplayer.now.com/nowplayer.now.com.config.js
@@ -18,6 +18,8 @@ module.exports = {
       return {
         Cookie:
           `LANG=${channel.lang}; Expires=null; Path=/; Domain=nowplayer.now.com`,
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",
       };
     },
     timeout: 60000,
