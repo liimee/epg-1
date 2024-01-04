54d71ca639d84e6dfc554d4f22cba58ee75a8072
liimee <70614549+liimee@users.noreply.github.com>
2024-01-04 20:56:20 +0700

    Update astro.com.my.config.js

--- sites/astro.com.my/astro.com.my.config.js
+++ sites/astro.com.my/astro.com.my.config.js
@@ -19,6 +19,10 @@ module.exports = {
       const start = dayjs.utc(item.datetimeInUtc);
       if(channel.xmltv_id == "LifetimeAsia.us") {
 	      start.subtract(10, "m")
+			} else if(channel.xmltv_id == "DiscoveryChannelIndonesia.id") {
+			  start.subtract(3, "m")
+			} else if(channel.xmltv_id == "CrimePlusInvestigationAsia.sg") {
+				start.subtract(5, "m")
 			}
       const duration = parseDuration(item.duration);
       const stop = start.add(duration, "s");
